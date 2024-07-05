04569e14c3f038bf6fde5216f43e5653fe543d07
Alejandro Hernandez <azure.satellite@gmail.com>
2024-07-05 11:07:35 -0400

    Add remix

---
+++ packages/remix/vite.config.ts
@@ -0,0 +1,24 @@
+import { vitePlugin as remix } from "@remix-run/dev"
+import hq from "alias-hq"
+import browserslistToEsbuild from "browserslist-to-esbuild"
+import { defineConfig } from "vite"
+
+// https://browsersl.ist
+const target = browserslistToEsbuild("defaults")
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    remix({
+      appDirectory: "src",
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+  ],
+  resolve: { alias: hq.get("rollup") },
+  optimizeDeps: { esbuildOptions: { target } },
+  build: { target },
+})
